18bf141b4df16022208e0763ea67e9cde21add7f
duchuyl <duchuy.le@student.unimelb.edu.au>
2023-04-04 17:37:39 +1000

    update code

--- search/program.py
+++ search/program.py
@@ -17,7 +17,7 @@ def distance(goal: tuple, point: tuple):
             coordinate = [goal[0] + i, goal[1] + j]
             if (min_distance > linear_distance(point, coordinate)):
                 min_distance =  linear_distance(point, coordinate)
-    return min_distance  
+    return min_distance
 
 def heuristic(start, goals):
     result = 0
@@ -73,20 +73,21 @@ def search(input: dict[tuple, tuple]) -> list[tuple]:
     
     direction = []
     # start loop 
-    test_result = []
+    result_coordinate = []
     while blues:
         curr = open_lst.get()
-        test_result.append(curr[1])
-        # generate moves and choose optimal moves
-        moves = expand(curr[1], reds[curr[1]][1])
+        result_coordinate.append(curr[1])
+        # generate neighbors
+        neighbors = expand(curr[1], reds[curr[1]][1])
         reds.pop(curr[1])
         min_h = MAX_HEURISTIC
         next_move = []
-        power = 0
         reach = {}
-        for move in moves:
+
+        # choose best move between neighbours 
+        for move in neighbors:
             for node in move:
-                if blues.get(node):
+                if blues.get(node) and blues.get(node)[1] != 6:
                     reach[node] = 1
                 else:
                     reach[node] = 0
@@ -95,7 +96,7 @@ def search(input: dict[tuple, tuple]) -> list[tuple]:
                         min_h = h
                         next_move = move
         reachable = 0
-        for move in moves:
+        for move in neighbors:
             sum = 0
             for node in move: 
                 sum += reach[node]
@@ -104,19 +105,19 @@ def search(input: dict[tuple, tuple]) -> list[tuple]:
                 next_move = move
         
         # update reds 
-        
         for item in next_move:
             if blues.get(item):
                 test = blues.pop(item)
-                reds[item] = ('r', 1 + test[1])
+                if test[1] != 6:
+                    reds[item] = ('r', 1 + test[1])
             else: 
                 reds[item] = ('r', 1)
             parent[item] = curr[1]
             open_lst.put((heuristic(item, blues), item))
-        direction.append(HEX_NEIGHBORS[moves.index(next_move)])
+        direction.append(HEX_NEIGHBORS[neighbors.index(next_move)])
     result = []
-    for i in range(0, len(test_result)):
-        result.append((test_result[i][0], test_result[i][1], direction[i][0], direction[i][1]))
+    for i in range(0, len(result_coordinate)):
+        result.append((result_coordinate[i][0], result_coordinate[i][1], direction[i][0], direction[i][1]))
     # Here we're returning "hardcoded" actions for the given test.csv file.
     # Of course, you'll need to replace this with an actual solution...
     return result
